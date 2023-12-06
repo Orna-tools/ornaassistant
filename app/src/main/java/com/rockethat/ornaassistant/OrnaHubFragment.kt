33b7813285e89adfa35dd110b0faa4f308162537
lloir <github@0x3d.uk>
2023-12-06 10:05:47 +0000

    Cleaning imports

--- app/src/main/java/com/rockethat/ornaassistant/OrnaHubFragment.kt
+++ app/src/main/java/com/rockethat/ornaassistant/OrnaHubFragment.kt
@@ -1,16 +1,15 @@
 package com.rockethat.ornaassistant
 
-import android.graphics.Bitmap
-import android.net.http.SslError
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.webkit.*
-import androidx.core.content.ContextCompat.startActivity
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
-import android.content.Intent
-import android.net.Uri
 
 class OrnaHubFragment : Fragment() {
 
