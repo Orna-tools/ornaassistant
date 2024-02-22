6859ea2e473ef36215844727de81a651f5ec85a0
THIS IS MY NAME <github@0x3d.uk>
2024-02-22 19:10:11 +0000

    Add OrnaTower and OrnaGuide web view activities
    The commit introduces two new activities - OrnaTowerActivity and OrnaGuideActivity which provide embedded web views for the respective URLs. It also adds buttons to navigate to these new activities. The 'Version Lloir' item is removed from the settings menu and a new permission for posting notifications is included. Furthermore, the viewBinding feature is enabled in build.gradle.

---
+++ app/src/main/java/com/rockethat/ornaassistant/OrnaTowerActivity.kt
@@ -0,0 +1,54 @@
+package com.rockethat.ornaassistant
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class OrnaTowerActivity : AppCompatActivity() {
+    companion object {
+        private const val ORNA_HUB_URL = "https://tower.fqegg.top/"
+        private const val ORNA_HUB_HOST = "https://tower.fqegg.top/"
+    }
+    private lateinit var ornaGuideWebView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ornahub)
+        ornaGuideWebView = findViewById(R.id.webview)
+        setupOrnaGuideWebView()
+        ornaGuideWebView.loadUrl(ORNA_HUB_URL)
+    }
+
+    private fun setupOrnaGuideWebView() {
+        ornaGuideWebView.settings.apply {
+            javaScriptEnabled = true
+            domStorageEnabled = false
+        }
+        ornaGuideWebView.webViewClient = createOrnaGuideWebViewClient()
+    }
+
+    private fun createOrnaGuideWebViewClient() = object : WebViewClient() {
+        override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+            val url = request.url.toString()
+            if (isRequestHostSameAsOrnaHubHost(url)) {
+                return false
+            }
+            launchExternalBrowser(url)
+            return true
+        }
+    }
+
+    private fun isRequestHostSameAsOrnaHubHost(url: String): Boolean {
+        return Uri.parse(url).host == ORNA_HUB_HOST
+    }
+
+    private fun launchExternalBrowser(url: String) {
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
+            startActivity(this)
+        }
+    }
+}
