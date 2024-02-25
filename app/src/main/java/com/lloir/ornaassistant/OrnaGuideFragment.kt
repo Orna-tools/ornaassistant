f1b0ede3da3584ee2e0f53272130e61d89306d71
THIS IS MY NAME <github@0x3d.uk>
2024-02-25 19:36:33 +0000

    Refactor activities to fragments and add notification settings
    This commit refactors OrnaGuideActivity and OrnaTowerActivity to fragments. It also adds a settings dialog for enabling notifications and modifies MainActivity to handle page transitions more efficiently. It changes the "background_color" in colors.xml and fixes minor other issues in SettingFragment and MainActivity.

--- app/src/main/java/com/lloir/ornaassistant/OrnaGuideFragment.kt
+++ app/src/main/java/com/lloir/ornaassistant/OrnaGuideFragment.kt
@@ -3,23 +3,33 @@ package com.lloir.ornaassistant
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 
-class OrnaGuideActivity : AppCompatActivity() {
+class OrnaGuideFragment : Fragment() {
+
     companion object {
-        private const val ORNA_HUB_URL = "https://orna.guide"
+        private const val ORNA_HUB_URL = "https://orna.guide/"
         private const val ORNA_GUIDE_HOST = "www.orna.guide"
     }
 
     private lateinit var ornaGuideWebView: WebView
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_ornahub)
-        ornaGuideWebView = findViewById(R.id.webview)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.activity_ornahub, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        ornaGuideWebView = view.findViewById(R.id.webview)
         setupOrnaGuideWebView()
         ornaGuideWebView.loadUrl(ORNA_HUB_URL)
     }
@@ -47,5 +57,9 @@ class OrnaGuideActivity : AppCompatActivity() {
         Intent(Intent.ACTION_VIEW, Uri.parse(url)).apply {
             startActivity(this)
         }
+    }
+
+    fun refreshWebpage() {
+        ornaGuideWebView.reload()
     }
 }
