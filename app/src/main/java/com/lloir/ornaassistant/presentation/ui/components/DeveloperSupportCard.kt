030286f8e8a65a156b8c41b7c3e9c91b674e479f
rcrosbie <1092cameo@gmail.com>
2025-07-04 11:48:33 -0400

    Refactor: Fix clipping of content in the Developer Support Card
    Extract the developer support content into a new composable function `DeveloperSupportCard`
    
    The button will wrap the content instead of having a hardcoded size.
    
    The updated layout is based on the material3 library sample `ButtonWithIconSample`

---
+++ app/src/main/java/com/lloir/ornaassistant/presentation/ui/components/DeveloperSupportCard.kt
@@ -0,0 +1,80 @@
+package com.lloir.ornaassistant.presentation.ui.components
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Devices.PIXEL_6_PRO
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.lloir.ornaassistant.presentation.theme.OrnaAssistantTheme
+
+@Composable
+fun DeveloperSupportCard(
+    modifier: Modifier = Modifier,
+    onDonateButtonClicked: () -> Unit
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Developed by lloir. If you wish, you can support the development by donating!",
+                style = MaterialTheme.typography.bodyMedium,
+                textAlign = TextAlign.Center
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Button(
+                onClick = onDonateButtonClicked,
+                contentPadding = ButtonDefaults.ButtonWithIconContentPadding
+            ) {
+                Icon(
+                    Icons.Default.Favorite,
+                    contentDescription = null,
+                    modifier = Modifier.size(ButtonDefaults.IconSize)
+                )
+                Spacer(modifier = Modifier.width(ButtonDefaults.IconSpacing))
+                Text("Donate via buymeacoffee")
+            }
+        }
+    }
+}
+
+
+@Preview(device = PIXEL_6_PRO)
+@Composable
+fun DeveloperSupportPreview() {
+    OrnaAssistantTheme {
+        DeveloperSupportCard(onDonateButtonClicked = {})
+    }
+}
+
+@Preview(device = PIXEL_6_PRO)
+@Composable
+fun DeveloperSupportPreviewWidth() {
+    OrnaAssistantTheme {
+        DeveloperSupportCard(modifier = Modifier.width(250.dp), onDonateButtonClicked = {})
+    }
+}
